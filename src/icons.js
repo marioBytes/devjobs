75f01e7c89971f37ae7e0408fb0bec30aaabea4a
marioBytes <mario.bytes92@gmail.com>
2022-10-03 16:59:49 -0600

    Create file that holds all logo files

---
+++ src/icons.js
@@ -0,0 +1,27 @@
+import Blogr from './assets/logos/blogr.svg';
+import CoffeeRoasters from './assets/logos/coffeeroasters.svg';
+import Creative from './assets/logos/creative.svg';
+import CrowdFund from './assets/logos/crowdfund.svg';
+import Maker from './assets/logos/maker.svg';
+import MasterCraft from './assets/logos/mastercraft.svg';
+import OfficeLite from './assets/logos/officelite.svg';
+import Pod from './assets/logos/pod.svg';
+import Pomodoro from './assets/logos/pomodoro.svg';
+import Scoot from './assets/logos/scoot.svg';
+import TypeMaster from './assets/logos/typemaster.svg';
+import Vector from './assets/logos/vector.svg';
+
+export {
+  Blogr,
+  CoffeeRoasters,
+  Creative,
+  CrowdFund,
+  Maker,
+  MasterCraft,
+  OfficeLite,
+  Pod,
+  Pomodoro,
+  Scoot,
+  TypeMaster,
+  Vector,
+};
